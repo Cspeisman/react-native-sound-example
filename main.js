f575a2b55e5609c5cb0c0ae99a9e13491a57f48e
Cspeisman <cspeisman@gmail.com>
2016-04-09 18:47:00 -0400

    plays multiple files in sync with one another

--- main.js
+++ main.js
@@ -12,24 +12,46 @@ var Sound = require('react-native-sound');
 class MainView extends Component {
   constructor() {
     super();
-    this.s = new Sound('Drum1.mp3', Sound.MAIN_BUNDLE, (e) => {
-      if (e) {
-        console.log('error', e);
-      } else {
-        console.log('duration', this.s.getDuration());
-      }
+
+    // creates an array for sound objects
+    const files = new Array(8).fill(undefined).map((value, index) => {
+      return new Sound(`MIX_${index + 1}.mp3`, Sound.MAIN_BUNDLE, (e) => {
+        if (e) console.log('error', e);
+      });
     });
-  }
-  render() {
-    return <View style={styles.container}>
-             <TouchableOpacity onPress={this.playSound.bind(this)}>
-               <Text style={styles.button}>play</Text>
-             </TouchableOpacity>
-           </View>;
+
+    this.state = {files: files}
   }
 
-  playSound() {
-    this.s.play()
+
+  render() {
+    const playSliders = this.state.files.map((value, index) => {
+      return (
+        <TouchableOpacity key={index} onPress={this.playSound.bind(this, index)}>
+          <Text style={styles.button}>play</Text>
+        </TouchableOpacity>
+      );
+    });
+
+    return (
+      <View style={styles.container}>
+        {playSliders}
+      </View>);
+  }
+
+  playSound(index) {
+    // creates new date object
+    const date = new Date();
+
+    // calculates how many milliseconds till the 0 millisecond of the next second
+    let milliseconds = 1000 - date.getMilliseconds();
+
+    setTimeout(() => {
+      this.state.files[index]
+        .setCurrentTime(date.getSeconds())
+        .setNumberOfLoops(-1)
+        .play()
+      }, milliseconds)
   }
 }
 
@@ -44,6 +66,7 @@ var styles = StyleSheet.create({
     fontSize: 20,
     backgroundColor: 'silver',
     padding: 5,
+    margin: 10,
   },
 });
 
